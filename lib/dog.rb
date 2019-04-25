9f10708930e318778faaf6d4a2509eb8b404cb1f
Ethan Sagin <ethansagin@gmail.com>
2019-04-25 14:20:51 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  @@all = []
+  
+  def self.clear_all
+    @@all.clear
+  end
+  
+  def self.all
+end
